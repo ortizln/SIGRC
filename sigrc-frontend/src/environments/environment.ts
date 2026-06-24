594e427ecba330cd062646e2f0da64a7d8a0db09
ortizln <ortizln9@gmail.com>
2026-06-24 13:40:05 -0500

    update

--- sigrc-frontend/src/environments/environment.ts
+++ sigrc-frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api/v1',
+  apiUrl: 'http://192.168.100.215:8080/api/v1',
   appName: 'SIGRC',
   appVersion: '1.0.0'
 };
