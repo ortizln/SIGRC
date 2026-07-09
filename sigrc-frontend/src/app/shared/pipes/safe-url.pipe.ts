9b0a5ac5a407341877e076dc1fedc273d3cd9b52
ortizln <ortizln9@gmail.com>
2026-07-09 13:24:42 -0500

    update

--- sigrc-frontend/src/app/shared/pipes/safe-url.pipe.ts
+++ sigrc-frontend/src/app/shared/pipes/safe-url.pipe.ts
@@ -4,7 +4,7 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 @Pipe({ name: 'safeUrl', standalone: true })
 export class SafeUrlPipe implements PipeTransform {
   constructor(private sanitizer: DomSanitizer) {}
-  transform(url: string): SafeResourceUrl {
-    return this.sanitizer.bypassSecurityTrustResourceUrl(url);
+  transform(url: string | null): SafeResourceUrl | null {
+    return url ? this.sanitizer.bypassSecurityTrustResourceUrl(url) : null;
   }
 }
